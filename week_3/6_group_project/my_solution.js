5c8b19206cba7a0f440ca35e4febf4660698ed01
Alex Le <alextle0125@gmail.com>
2014-05-29 21:31:45 -0700

    added reflection

--- week_3/6_group_project/my_solution.js
+++ week_3/6_group_project/my_solution.js
@@ -1,3 +1,20 @@
-// My role in the group is:
+// My role in the group is: Person 3 - Translate Pseudocode into Code
 
-// Here is my part of the challenge:
+// __________________________________________
+// Reflection: Use the reflection guidelines to write a reflection here. 
+// My strategy here was to trust my group-mates and of course have tons of Javascript references on deck as I ran and 
+// checked my code in node.js' interactive mode. I worked through each pseudocode individually with the 
+// javascript_intro_lab documentation and w3schools documentation to guide me. I also reviewed prior challenges for 
+// guidance with the assertion tasks (as I am not very experienced creating assertions). I ran into some issues with
+// syntax, but managed to work through them with the resources I referred to earlier. A specific function that I had
+// issues and concerns about was a median finding function - that according to the pseudocode, resulted in a variety 
+// of outputs depending upon the structure and items of an array. It didn't quite fit my understanding of the median
+// value; but I decided to trust my group, and output a solution that would satisfy the pseudocode tests. This challenge
+// definitely tested my knowledge of Javascript and Javascript syntax. I learned a lot of operational techniques and feel
+// much more comfortable writing assertions. I feel mild-moderately confident in my abilities after this challenge; as I
+// know that I will still need to frequently reference documentation and review past coding. I enjoyed working with more
+// complicated material and developing my JS skills. I didn't find any of this tedious. I find that doing this type of
+// group development requires every member to be on-point. I definitely felt some pressure - not to let the group down.
+// I guess that might be a good thing. I also think this type of team development requires some intuition into what your
+// partner(s) are expecting and intuition on what your partner(s) may be missing. My portion of the project involved the
+// actual coding. It was heavily logic based but definitely was advantageous in developing my JS knowledge.
